ff743e3e9bc80e118957558eb708ebc3d49f20ec
Nurlan <nurlan.grachyov@mail.ru>
2024-10-19 23:36:53 +0400

    Готово

--- tests/test_main.py
+++ tests/test_main.py
@@ -9,3 +9,5 @@ def test_category_init(category):
     assert category.name == "shoes"
     assert category.description == "winter shoes"
     assert category.products == ["boots with fur", "winter jacket"]
+    assert category.category_count == 1
+    assert category.product_count == 2
