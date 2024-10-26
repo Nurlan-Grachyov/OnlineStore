73c0bdc9e02e8c3964b14d6d62df1733927e6ed3
Nurlan <nurlan.grachyov@mail.ru>
2024-10-26 19:31:25 +0400

    Update

--- src/category.py
+++ src/category.py
@@ -1,3 +1,6 @@
+from src.product import Product
+
+
 class Category:
     name: str
     description: str
@@ -10,15 +13,33 @@ class Category:
         Category.product_count += len(products)
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products
+
+    @property
+    def return_product(self):
+        return [f"Название продукта : {product.name}, цена : {product.check_price} рублей, Остаток: {product.quantity} штук."  for product in self.__products]
+
+    def add_product(self, product):
+        self.__products.append(product)
+
+    @property
+    def products(self):
+        return self.__products
+
+    @products.setter
+    def products(self, new_products):
+        self.__products = new_products
 
     def __str__(self):
-        return f"Category(name={self.name}," f"description={self.description}," f"products={self.products})"
+        products_str = ", ".join(str(product) for product in self.__products)
+        return f"Category(name={self.name}," f"description={self.description}," f"products={[{products_str}]})"
 
 if __name__ == "__main__":
-    category_1 = Category("products", "products for salad", ["tomato", "cucumber", "onion"])
-    category_2 = Category("sweets", "sweet", ["chocolate", "candies"])
-    print(category_1)
-    print(category_2)
-    print(Category.category_count)
-    print(Category.product_count)
+    product_1 = Product("tomato", "red tomato from Azerbaijan", 150, 10)
+    product_2 = Product("cucumber", "cucumber from Azerbaijan", 100, 20)
+    category_1 = Category("products", "products for salad", [product_1, product_2])
+    product_3 = Product("lettuce", "fresh lettuce", 50, 15)
+    category_1.add_product(product_3)
+    # print(category_1.return_product)
+
+
--- src/product.py
+++ src/product.py
@@ -7,21 +7,52 @@ class Product:
     def __init__(self, name, description, price, quantity):
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
+
+    @property
+    def check_price(self):
+        return self.__price
+
+    @check_price.setter
+    def check_price(self, price):
+        if price <= 0:
+            print(f"Цена не должна быть нулевая или отрицательная")
+        else:
+            self.__price = price
+
+    @classmethod
+    def new_product(cls, product):
+        name =  product['name']
+        description = product['description']
+        price = product['price']
+        quantity = product['quantity']
+        return  cls(name, description, price, quantity)
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, new_price):
+        self.__price = new_price
 
     def __str__(self):
         return (
             f"Product(name={self.name}, "
             f"description={self.description}, "
-            f"price={self.price}, "
+            f"price={self.__price}, "
             f"quantity={self.quantity})"
         )
 
-
-
 if __name__ == "__main__":
-    product_1 = Product("tomato", "red tomato from Azerbaijan", 150, 10)
-    product_2 = Product("cucumber", "cucumber from Azerbaijan", 100, 20)
-    print(product_1)
-    print(f"{product_2}\n")
+    # product_1 = Product("tomato", "red tomato from Azerbaijan", 150, 10)
+    # product_2 = Product("cucumber", "cucumber from Azerbaijan", 100, 20)
+    # print(product_1)
+    # print(f"{product_2}\n")
+    product_1 = {"name" : "Помидоры", "description" : "Помидоры красные, вкусные, не дорого", "price" : 170, "quantity" : 10 }
+    product_add = Product.new_product(product_1)
+    print(product_add)
+    product_add.check_price = -5
+    product_add.check_price = 20
+    print(product_add)
